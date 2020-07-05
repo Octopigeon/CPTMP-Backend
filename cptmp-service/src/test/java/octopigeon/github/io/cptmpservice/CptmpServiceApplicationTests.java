b8561253d674188f95498637eea52b3d879b85d7
Chong Wayne <eddyw2222@gmail.com>
2020-07-06 00:06:28 +0800

    :tada:
    首次commit，增加用于环境测试的Pigeon服务

---
+++ cptmp-service/src/test/java/octopigeon/github/io/cptmpservice/CptmpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package octopigeon.github.io.cptmpservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CptmpServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
