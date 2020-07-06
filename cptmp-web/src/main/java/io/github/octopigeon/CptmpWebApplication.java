9f82e2bc4944d704f2a4b43a15152afc39309516
Chong Wayne <eddyw2222@gmail.com>
2020-07-06 13:47:02 +0800

    :package: :hammer:
    反写包名和groupid

---
+++ cptmp-web/src/main/java/io/github/octopigeon/CptmpWebApplication.java
@@ -0,0 +1,13 @@
+package io.github.octopigeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CptmpWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CptmpWebApplication.class, args);
+    }
+
+}
---
+++ cptmp-web/src/test/java/io/github/octopigeon/cptmpweb/CptmpWebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.octopigeon.cptmpweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CptmpWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
