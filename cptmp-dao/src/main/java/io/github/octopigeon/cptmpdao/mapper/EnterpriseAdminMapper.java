ef2bba54e7d0341bdad8e5b76c7b90cad900a986
G-H-Li <2215228145@qq.com>
2020-07-08 00:42:15 +0800

    📦 🔨

--- cptmp-dao/src/main/java/io/github/octopigeon/cptmpdao/mapper/EnterpriseAdminMapper.java
+++ cptmp-dao/src/main/java/io/github/octopigeon/cptmpdao/mapper/EnterpriseAdminMapper.java
@@ -18,16 +18,35 @@ import java.util.List;
 @Repository
 @Mapper
 public interface EnterpriseAdminMapper {
-    @Insert("insert into enterprise_admin (gmt_create, idx_name, uk_user_id, uk_employee_id) values (#{gmtCreate, name, userId, employeeId)")
+    /**
+     * 向enterprise admin中插入一条数据
+     * @param enterpriseAdmin：类
+     */
+    @Insert("insert into enterprise_admin (gmt_create, idx_name, uk_user_id, uk_employee_id) values (#{gmtCreate}, #{name}, #{userId}, #{employeeId})")
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
     void addEnterpriseAdmin(EnterpriseAdmin enterpriseAdmin);
 
+    /**
+     * 根据userId删除对应的企业管理员信息
+     * @param userId：用户id
+     */
     @Delete("delete from enterprise_admin where uk_user_id = #{userId}")
     void removeEnterprseAdminByUserId(BigInteger userId);
 
+    /**
+     * 根据用户id更新数据
+     * @param userId：用户id
+     * @param gmtModified：更新时间
+     * @param name：员工名称
+     * @param employeeId：员工工号
+     */
     @Update("update enterprise_admin set gmt_modified = #{gmtModified}, idx_name = #{name}, uk_employee_id = #{employeeId} where uk_user_id = #{userId}")
     void updateEnterprseAdminByUserId(BigInteger userId, Date gmtModified, String name, BigInteger employeeId);
 
+    /**
+     * 查找所有的企业管理员的账号、姓名、工号
+     * @return 企业管理员列表
+     */
     @Select("select uk_user_id, idx_name, uk_employee_id from enterprise_admin")
     @Results({
             @Result(column = "uk_user_id", property = "userId", jdbcType = JdbcType.BIGINT),
@@ -36,8 +55,14 @@ public interface EnterpriseAdminMapper {
     })
     List<EnterpriseAdmin> findAllEnterpriseAdmins();
 
-    @Select("select idx_name, uk_employee_id from enterprise_admin where uk_user_id = #{userId}")
+    /**
+     * 根据userId查找员工账号、姓名、工号
+     * @param userId：用户账号
+     * @return 企业管理员实例
+     */
+    @Select("select uk_user_id, idx_name, uk_employee_id from enterprise_admin where uk_user_id = #{userId}")
     @Results({
+            @Result(column = "uk_user_id", property = "userId", jdbcType = JdbcType.BIGINT),
             @Result(column = "idx_name", property = "name", jdbcType = JdbcType.VARCHAR),
             @Result(column = "uk_employee_id", property = "employeeId", jdbcType = JdbcType.BIGINT)
     })
--- cptmp-dao/src/main/java/io/github/octopigeon/cptmpdao/model/SchoolStudent.java
+++ cptmp-dao/src/main/java/io/github/octopigeon/cptmpdao/model/SchoolStudent.java
@@ -1,15 +1,13 @@
 package io.github.octopigeon.cptmpdao.model;
 
-
-import com.alibaba.fastjson.annotation.JSONField;
 import lombok.Data;
 
 import java.math.BigInteger;
 import java.util.Date;
 
 /**
- * @author anlow
- * @version 1.0
+ * @author anlow,Gh Li
+ * @version 1.1
  * @date 2020/7/7
  */
 @Data
@@ -20,8 +18,8 @@ public class SchoolStudent {
     private Date gmtModified;
     private String name;
     private String schoolName;
-    private String userId;
-    private String studentId;
+    private BigInteger userId;
+    private BigInteger studentId;
     /**
      * 学生证件照的URL，用于人脸比对从而实现人脸识别
      */
