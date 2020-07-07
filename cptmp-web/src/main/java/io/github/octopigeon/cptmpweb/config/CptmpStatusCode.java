3ba8db8ad8afb911ed34c2e8b8d33fa69703abd7
Chong Wayne <eddyw2222@gmail.com>
2020-07-08 01:23:22 +0800

    :book:
    增加状态码的常数类

---
+++ cptmp-web/src/main/java/io/github/octopigeon/cptmpweb/config/CptmpStatusCode.java
@@ -0,0 +1,15 @@
+package io.github.octopigeon.cptmpweb.config;
+
+/**
+ * @author anlow
+ * @version 1.0
+ * @date 2020/7/7
+ */
+public class CptmpStatusCode {
+
+    public static int OK = 0;
+
+    /** 登录验证失败 */
+    public static int AUTH_FAILED = 1;
+
+}
