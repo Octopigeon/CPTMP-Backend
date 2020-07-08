810bde2ac9b24ab4a1b3f8a755bf0bd7a8c708f6
e <1072652010@qq.com>
2020-07-08 23:17:34 +0800

    日志数据库

--- cptmp-dao/src/main/java/io/github/octopigeon/cptmpdao/mapper/ActivityRecordMapper.java
+++ cptmp-dao/src/main/java/io/github/octopigeon/cptmpdao/mapper/ActivityRecordMapper.java
@@ -22,7 +22,7 @@ public interface ActivityRecordMapper {
      * 向activity record中插入一条数据
      * @param activityRecord：类
      */
-    @Insert("insert into activity_record (gmt_create, state_record, event_record, uk_people_id, uk_team_id) values (#{gmtCreate}, #{state}, #{event}, #{peopleId}, #{teamId})")
+    @Insert("insert into activity_record (gmt_create, state_record, event_record, uk_user_id, uk_team_id) values (#{gmtCreate}, #{state}, #{event}, #{userId}, #{teamId})")
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
     void addActivityRecord(ActivityRecord activityRecord);
 
@@ -33,10 +33,10 @@ public interface ActivityRecordMapper {
 //    void removeActivityRecordByAll();
 //    /**
 //     * 根据活动人id删除对应的活动信息
-//     * @param peopleId：活动人id
+//     * @param userId：活动人id
 //     */
-//    @Delete("delete from activity_record where uk_people_id = #{peopleId}")
-//    void removeActivityRecordByPeopleId(BigInteger peopleId);
+//    @Delete("delete from activity_record where uk_user_id = #{userId}")
+//    void removeActivityRecordByUserId(BigInteger userId);
 //
 //    /**
 //     * 根据团队伍id删除对应的活动信息
@@ -48,25 +48,25 @@ public interface ActivityRecordMapper {
 //
 //    /**
 //     * 根据活动人id修改活动记录
-//     * @param peopleId：活动人id
+//     * @param userId：活动人id
 //     * @param teamId：团队id
 //     * @param gmtModified：修改时间
 //     * @param state：活动状态
 //     * @param event：活动内容事件
 //     */
-//    @Update("update activity_record set gmt_modified = #{gmtModified}, state_record = #{state}, event_record = #{event}, uk_people_id = #{peopleId}, uk_team_id = #{teamId} where uk_people_id = #{peopleId}")
-//    void updateActivityRecordByPeopleId(BigInteger peopleId, BigInteger teamId, Date gmtModified, BigInteger state, String event);
+//    @Update("update activity_record set gmt_modified = #{gmtModified}, state_record = #{state}, event_record = #{event}, uk_user_id = #{userId}, uk_team_id = #{teamId} where uk_user_id = #{userId}")
+//    void updateActivityRecordByUserId(BigInteger userId, BigInteger teamId, Date gmtModified, BigInteger state, String event);
 //
 //    /**
 //     *根据团队id修改活动记录
-//     * @param peopleId：活动人id
+//     * @param userId：活动人id
 //     * @param teamId：团队id
 //     * @param gmtModified：修改时间
 //     * @param state：活动状态
 //     * @param event：活动内容事件
 //     */
-//    @Update("update activity_record set gmt_modified = #{gmtModified}, state_record = #{state}, event_record = #{event}, uk_people_id = #{peopleId}, uk_team_id = #{teamId} where uk_team_id = #{teamId}")
-//    void updateActivityRecordByTeamId(BigInteger peopleId, BigInteger teamId, Date gmtModified, BigInteger state, String event);
+//    @Update("update activity_record set gmt_modified = #{gmtModified}, state_record = #{state}, event_record = #{event}, uk_user_id = #{userId}, uk_team_id = #{teamId} where uk_team_id = #{teamId}")
+//    void updateActivityRecordByTeamId(BigInteger userId, BigInteger teamId, Date gmtModified, BigInteger state, String event);
 
     /**
      * 查找所有活动记录
@@ -75,29 +75,31 @@ public interface ActivityRecordMapper {
     @Select("select * from activity_record")
     @Results({
             @Result(column = "id", property = "id", jdbcType = JdbcType.BIGINT),
-            @Result(column = "people_id", property = "peopleId", jdbcType = JdbcType.BIGINT),
-            @Result(column = "team_id", property = "teamId", jdbcType = JdbcType.BIGINT),
+            @Result(column = "uk_user_id", property = "userId", jdbcType = JdbcType.BIGINT),
+            @Result(column = "uk_team_id", property = "teamId", jdbcType = JdbcType.BIGINT),
             @Result(column = "event_record", property = "event", jdbcType = JdbcType.VARCHAR),
             @Result(column = "state_record", property = "state", jdbcType = JdbcType.BIGINT),
-            @Result(column = "gmt_create", property = "gmtCreate", jdbcType = JdbcType.DATE)
+            @Result(column = "gmt_create", property = "gmtCreate", jdbcType = JdbcType.DATE),
+            @Result(column = "gmt_modify", property = "gmtModify", jdbcType = JdbcType.DATE)
     })
     List<ActivityRecord> findAllActivityRecord();
 
     /**
      * 按照活动人id查找活动记录
-     * @param peopleId:活动人id
+     * @param userId:活动人id
      * @return 活动记录列表
      */
-    @Select("select * from activity_record where uk_people_id = #{peopleId}")
+    @Select("select * from activity_record where uk_user_id = #{userId}")
     @Results({
             @Result(column = "id", property = "id", jdbcType = JdbcType.BIGINT),
-            @Result(column = "people_id", property = "peopleId", jdbcType = JdbcType.BIGINT),
-            @Result(column = "team_id", property = "teamId", jdbcType = JdbcType.BIGINT),
+            @Result(column = "uk_user_id", property = "userId", jdbcType = JdbcType.BIGINT),
+            @Result(column = "uk_team_id", property = "teamId", jdbcType = JdbcType.BIGINT),
             @Result(column = "event_record", property = "event", jdbcType = JdbcType.VARCHAR),
             @Result(column = "state_record", property = "state", jdbcType = JdbcType.BIGINT),
-            @Result(column = "gmt_create", property = "gmtCreate", jdbcType = JdbcType.DATE)
+            @Result(column = "gmt_create", property = "gmtCreate", jdbcType = JdbcType.DATE),
+            @Result(column = "gmt_modify", property = "gmtModify", jdbcType = JdbcType.DATE)
     })
-    ActivityRecord findActivityRecordByPeopleId(BigInteger peopleId);
+    List<ActivityRecord> findActivityRecordByUserId(BigInteger userId);
 
     /**
      * 按照团队id查找活动记录
@@ -107,13 +109,14 @@ public interface ActivityRecordMapper {
     @Select("select * from activity_record where uk_team_id = #{teamId}")
     @Results({
             @Result(column = "id", property = "id", jdbcType = JdbcType.BIGINT),
-            @Result(column = "people_id", property = "peopleId", jdbcType = JdbcType.BIGINT),
-            @Result(column = "team_id", property = "teamId", jdbcType = JdbcType.BIGINT),
+            @Result(column = "uk_user_id", property = "userId", jdbcType = JdbcType.BIGINT),
+            @Result(column = "uk_team_id", property = "teamId", jdbcType = JdbcType.BIGINT),
             @Result(column = "event_record", property = "event", jdbcType = JdbcType.VARCHAR),
             @Result(column = "state_record", property = "state", jdbcType = JdbcType.BIGINT),
-            @Result(column = "gmt_create", property = "gmtCreate", jdbcType = JdbcType.DATE)
+            @Result(column = "gmt_create", property = "gmtCreate", jdbcType = JdbcType.DATE),
+            @Result(column = "gmt_modify", property = "gmtModify", jdbcType = JdbcType.DATE)
     })
-    ActivityRecord findActivityRecordByTeamId(BigInteger teamId);
+    List<ActivityRecord> findActivityRecordByTeamId(BigInteger teamId);
 
 }
 
---
+++ cptmp-dao/src/main/java/io/github/octopigeon/cptmpdao/mapper/DailyRecordMapper.java
@@ -0,0 +1,109 @@
+package io.github.octopigeon.cptmpdao.mapper;
+
+import io.github.octopigeon.cptmpdao.model.DailyRecord;
+import io.github.octopigeon.cptmpdao.model.EnterpriseAdmin;
+import lombok.Data;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.type.JdbcType;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author 李国鹏
+ * @version 1.0
+ * @date 2020/7/8
+ * <p>
+ * last-check-in 李国鹏
+ * @date 2020/7/8
+ */
+@Repository
+@Mapper
+public interface DailyRecordMapper {
+    /**
+     * 添加日志
+     * @param dailyRecord：日志类
+     */
+    @Insert("insert into daily_record (gmt_create, title, content, uk_user_id, document_path, record_type) values (#{gmtCreate}, #{title}, #{content}, #{userId}, #{documentPath}, #{recordType})")
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+    void addDailyRecord(DailyRecord dailyRecord);
+
+    /**
+     * 根据日志id删除对应的日志信息
+     * @param id：日志id
+     */
+    @Delete("delete from daily_record where id = #{id}")
+    void removeDailyRecordById(BigInteger id);
+
+    /**
+     * 根据用户id删除日志
+     * @param userId 用户id
+     */
+    @Delete("delete from daily_record where uk_user_id = #{userId}")
+    void removeDailyRecordByUserId(BigInteger userId);
+
+    /**
+     * 根据日志id修改日志
+     * @param id 日志id
+     * @param gmtModified 修改时间
+     * @param title 标题
+     * @param content 内容
+     * @param userId 用户id
+     * @param documentPath 文件路径
+     * @param recordType 文件类型
+     */
+    @Update("update daily_record set gmt_modified = #{gmtModified}, title = #{title}, content = #{content}, uk_user_id = #{userId}, document_path = #{documentPath}, record_type = #{recordType} where id = #{id}")
+    void updateDailyRecordById(BigInteger id, Date gmtModified, String title, String content, BigInteger userId, String documentPath, int recordType);
+
+    /**
+     * 根据用户id修改日志
+     * @param gmtModified 修改时间
+     * @param title 标题
+     * @param content 内容
+     * @param userId 用户id
+     * @param documentPath 文件路径
+     * @param recordType 文件类型
+     */
+    @Update("update daily_record set gmt_modified = #{gmtModified}, title = #{title}, content = #{content}, uk_user_id = #{userId}, document_path = #{documentPath}, record_type = #{recordType} where uk_user_id = #{userId}")
+    void updateDailyRecordByUserId(Date gmtModified, String title, String content, BigInteger userId, String documentPath, int recordType);
+
+    /**
+     * 查询所有日志
+     * @return 日志列表
+     */
+    @Select("select * from daily_record")
+    @Results({
+            @Result(column = "id", property = "id", jdbcType = JdbcType.BIGINT),
+            @Result(column = "gmt_create", property = "gmtCreate", jdbcType = JdbcType.DATE),
+            @Result(column = "gmt_modify", property = "gmtModify", jdbcType = JdbcType.DATE),
+            @Result(column = "uk_user_id", property = "userId", jdbcType = JdbcType.BIGINT),
+            @Result(column = "document_path", property = "documentPath", jdbcType = JdbcType.VARCHAR),
+            @Result(column = "record_type", property = "recordType", jdbcType = JdbcType.INTEGER),
+            @Result(column = "title", property = "title", jdbcType = JdbcType.VARCHAR),
+            @Result(column = "content", property = "content", jdbcType = JdbcType.VARCHAR)
+
+    })
+    List<DailyRecord> findAllDailyRecord();
+
+    /**
+     * 根据用户id查找日志
+     * @param userId：用户id
+     * @return 日志
+     */
+    @Select("select * from daily_record where uk_user_id = #{userId}")
+    @Results({
+            @Result(column = "id", property = "id", jdbcType = JdbcType.BIGINT),
+            @Result(column = "gmt_create", property = "gmtCreate", jdbcType = JdbcType.DATE),
+            @Result(column = "gmt_modify", property = "gmtModify", jdbcType = JdbcType.DATE),
+            @Result(column = "uk_user_id", property = "userId", jdbcType = JdbcType.BIGINT),
+            @Result(column = "document_path", property = "documentPath", jdbcType = JdbcType.VARCHAR),
+            @Result(column = "record_type", property = "recordType", jdbcType = JdbcType.INTEGER),
+            @Result(column = "title", property = "title", jdbcType = JdbcType.VARCHAR),
+            @Result(column = "content", property = "content", jdbcType = JdbcType.VARCHAR)
+    })
+    List<DailyRecord> findDailyRecordByUserId(BigInteger userId);
+
+
+}
