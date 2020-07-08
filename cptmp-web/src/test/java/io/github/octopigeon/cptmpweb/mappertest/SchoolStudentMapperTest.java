b360d041c66fdbb70f5f98f5cf51e646fb2f2ed3
GuoHaoLi <2215228145@qq.com>
2020-07-08 13:19:33 +0800

    :white_check_mark:
    添加对SchoolStudentmapper的测试

---
+++ cptmp-web/src/test/java/io/github/octopigeon/cptmpweb/mappertest/SchoolStudentMapperTest.java
@@ -0,0 +1,15 @@
+package io.github.octopigeon.cptmpweb.mappertest;
+
+import io.github.octopigeon.cptmpweb.BaseTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author Gh Li
+ * @version 1.0
+ * @date 2020/7/8
+ * @last-check-in Gh Li
+ * @date 2020/7/8
+ */
+public class SchoolStudentMapperTest extends BaseTest {
+    @Autowired
+}
