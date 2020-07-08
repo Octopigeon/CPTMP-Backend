f6ebe43f33cedbea8c2fd4d0f066a2d475239390
Chong Wayne <eddyw2222@gmail.com>
2020-07-08 12:56:15 +0800

    :card_index:
    修改User和UserMapper

--- cptmp-dao/src/main/java/io/github/octopigeon/cptmpdao/model/CptmpUser.java
+++ cptmp-dao/src/main/java/io/github/octopigeon/cptmpdao/model/CptmpUser.java
@@ -3,6 +3,7 @@ package io.github.octopigeon.cptmpdao.model;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Date;
 
@@ -10,20 +11,29 @@ import java.util.Date;
  * @author anlow
  * @version 1.0
  * @date 2020/7/7
+ *
+ * @last-check-in anlow
+ * @date 2020/7/8
  */
 @Data
 public class CptmpUser {
 
     private BigInteger id;
     private Date gmtCreate;
-    /** nullable */
-    private Date gmtModified;
     private String username;
     private String password;
+    private String email;
+    private String roleName;
+    private Boolean enabled;
+    private Boolean accountNonExpired;
+    private Boolean credentialsNonExpired;
+    private Boolean accountNonLocked;
+    /** nullable */
+    private Date gmtModified;
     /** nullable */
     private String introduction;
     /** nullable */
-    private String contactInfo;
+    private BigDecimal phoneNumber;
     /**
      * 0-female, 1-male
      * nullable
