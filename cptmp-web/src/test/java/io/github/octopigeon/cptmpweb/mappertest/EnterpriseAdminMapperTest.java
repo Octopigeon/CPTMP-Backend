66313d99bd35c7b1e2a77752d379f907f7f94d53
GuoHaoLi <2215228145@qq.com>
2020-07-08 13:19:33 +0800

    :white_check_mark:
    添加对SchoolStudentmapper的测试

---
+++ cptmp-web/src/test/java/io/github/octopigeon/cptmpweb/mappertest/EnterpriseAdminMapperTest.java
@@ -0,0 +1,11 @@
+package io.github.octopigeon.cptmpweb.mappertest;
+
+import io.github.octopigeon.cptmpweb.BaseTest;
+
+/**
+ * @author Gh Li
+ * @version 1.0
+ * @date 2020/7/8
+ */
+public class EnterpriseAdminMapperTest extends BaseTest  {
+}
