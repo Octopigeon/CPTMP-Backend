3267ec8daf3663c09761754c1df29081d34fb885
Eric_Lian <git@superexercisebook.com>
2020-07-08 10:36:48 +0800

    🚧 用户中心采用 BCrypt 存储用户密码

--- cptmp-dao/src/main/java/io/github/octopigeon/cptmpdao/model/User.java
+++ cptmp-dao/src/main/java/io/github/octopigeon/cptmpdao/model/User.java
@@ -1,25 +1,35 @@
 package io.github.octopigeon.cptmpdao.model;
 
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.math.BigInteger;
 import java.util.Date;
 
 /**
- * @author anlow
- * @version 1.0
+ * 用户模型
+ *
+ * @author anlow, Eric_Lian
+ * @version 1.1
  * @date 2020/7/7
  */
 @Data
 public class User {
+
+    private static final PasswordEncoder ENCODER = new BCryptPasswordEncoder();
 
     private BigInteger id;
     private Date gmtCreate;
     /** nullable */
     private Date gmtModified;
     private String username;
+
     private String password;
+
     /** nullable */
     private String introduction;
     /** nullable */
@@ -31,5 +41,18 @@ public class User {
     private Boolean male;
     /** nullable */
     private String avatar;
+
+    public User setPassword(String password) {
+        this.password = ENCODER.encode(password);
+        return this;
+    }
+
+    public boolean validatePassword(String submittedPassword) {
+        boolean ret = ENCODER.matches(submittedPassword, this.password);
+        if (ret) {
+            this.setPassword(submittedPassword);
+        }
+        return ret;
+    }
 
 }
