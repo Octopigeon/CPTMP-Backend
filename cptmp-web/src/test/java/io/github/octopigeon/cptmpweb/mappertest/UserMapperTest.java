c8793af46a1be610e05b0436aec78b550e743982
Eric_Lian <git@superexercisebook.com>
2020-07-08 10:36:48 +0800

    🚧 用户中心采用 BCrypt 存储用户密码

--- cptmp-web/src/test/java/io/github/octopigeon/cptmpweb/mappertest/UserMapperTest.java
+++ cptmp-web/src/test/java/io/github/octopigeon/cptmpweb/mappertest/UserMapperTest.java
@@ -11,8 +11,10 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * @author anlow
- * @version 1.0
+ * 用户中心测试单元
+ *
+ * @author anlow, Eric_Lian
+ * @version 1.1
  * @date 2020/7/8
  */
 public class UserMapperTest extends BaseTest {
@@ -26,6 +28,14 @@ public class UserMapperTest extends BaseTest {
         user1.setGmtCreate(new Date());
         user1.setUsername("test1");
         user1.setPassword("123456");
+
+        Assertions.assertEquals(false, user1.validatePassword("114514"));   // 验证错误密码
+        String user1pw1 = user1.getPassword();
+        Assertions.assertEquals(true, user1.validatePassword("123456"));    // 验证正确密码
+        String user1pw2 = user1.getPassword();
+        Assertions.assertEquals(false, user1pw1.equals(user1pw2));          // 验证密码自动更新
+        Assertions.assertEquals(true, user1.validatePassword("123456"));    // 验证正确密码
+
         User user2 = new User();
         user2.setGmtCreate(new Date());
         user2.setUsername("test2");
