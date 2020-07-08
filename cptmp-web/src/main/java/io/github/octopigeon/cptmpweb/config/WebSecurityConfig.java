d272256a81766c959e393177eea49e90243ce437
Chong Wayne <eddyw2222@gmail.com>
2020-07-08 17:38:19 +0800

    :lock:
    继续登陆验证

--- cptmp-web/src/main/java/io/github/octopigeon/cptmpweb/config/WebSecurityConfig.java
+++ cptmp-web/src/main/java/io/github/octopigeon/cptmpweb/config/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import io.github.octopigeon.cptmpservice.service.CptmpUserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @author anlow
@@ -29,37 +32,52 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private CptmpAuthenticationFailureHandlerImpl cptmpAuthenticationFailureHandler;
 
+    @Autowired
+    private CptmpUserDetailsServiceImpl cptmpUserDetailsService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        return new CptmpUserDetailsServiceImpl();
-    }
+//    @Bean
+//    @Override
+//    public UserDetailsService userDetailsService() {
+//        return new CptmpUserDetailsServiceImpl();
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .successHandler(cptmpAuthenticationSuccessHandler)
-                .failureHandler(cptmpAuthenticationFailureHandler)
-                //.loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/", "/home").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .successHandler(cptmpAuthenticationSuccessHandler)
+//                .failureHandler(cptmpAuthenticationFailureHandler)
+//                .loginPage("/login")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .permitAll();
+
+        // 配置filter
+        CptmpLoginAuthenticationFilter cptmpLoginAuthenticationFilter = new CptmpLoginAuthenticationFilter("/login", HttpMethod.POST.name());
+        cptmpLoginAuthenticationFilter.setAuthenticationManager(http.getSharedObject(AuthenticationManager.class));
+        cptmpLoginAuthenticationFilter.setAuthenticationSuccessHandler(cptmpAuthenticationSuccessHandler);
+        cptmpLoginAuthenticationFilter.setAuthenticationFailureHandler(cptmpAuthenticationFailureHandler);
+        // 配置provider
+        CptmpLoginAuthenticationProvider cptmpLoginAuthenticationProvider = new CptmpLoginAuthenticationProvider();
+        cptmpLoginAuthenticationProvider.setCptmpUserDetailsService(cptmpUserDetailsService);
+
+        http.authenticationProvider(cptmpLoginAuthenticationProvider).addFilterAfter(cptmpLoginAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class);
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
-    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+//    }
 
 }
