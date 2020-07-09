6d69bc0e122e1f0e603aad7b9e0730154e3f6d8c
GuoHaoLi <2215228145@qq.com>
2020-07-09 16:03:48 +0800

    :heavy_plus_sign:
    添加包依赖

---
+++ cptmp-web/src/test/java/io/github/octopigeon/cptmpweb/servicetest/RegistrationServiceTest.java
@@ -0,0 +1,13 @@
+package io.github.octopigeon.cptmpweb.servicetest;
+
+import io.github.octopigeon.cptmpweb.BaseTest;
+
+/**
+ * @author Gh Li
+ * @version 1.0
+ * @date 2020/7/9
+ * @last-check-in Gh Li
+ * @date 2020/7/9
+ */
+public class RegistrationServiceTest extends BaseTest {
+}
