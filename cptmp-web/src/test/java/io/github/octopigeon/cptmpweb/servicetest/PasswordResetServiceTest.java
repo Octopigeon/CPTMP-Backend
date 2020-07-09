b9d6c4654a1ab3a9ea65d2173b8f27485af7231d
Chong Wayne <eddyw2222@gmail.com>
2020-07-10 00:40:30 +0800

    :poop:
    密码token以及测试

--- cptmp-web/src/test/java/io/github/octopigeon/cptmpweb/servicetest/PasswordResetServiceTest.java
+++ cptmp-web/src/test/java/io/github/octopigeon/cptmpweb/servicetest/PasswordResetServiceTest.java
@@ -1,9 +1,10 @@
-package io.github.octopigeon.cptmpweb.mappertest;
+package io.github.octopigeon.cptmpweb.servicetest;
 
 import io.github.octopigeon.cptmpdao.mapper.CptmpUserMapper;
 import io.github.octopigeon.cptmpdao.mapper.PasswordResetTokenMapper;
 import io.github.octopigeon.cptmpdao.model.CptmpUser;
 import io.github.octopigeon.cptmpdao.model.PasswordResetToken;
+import io.github.octopigeon.cptmpservice.service.PasswordResetService;
 import io.github.octopigeon.cptmpweb.BaseTest;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
@@ -16,11 +17,11 @@ import java.util.UUID;
 /**
  * @author anlow
  * @version 1.0
- * @date 2020/7/9
+ * @date 2020/7/10
  * @last-check-in anlow
- * @date 2020/7/9
+ * @date 2020/7/10
  */
-public class PasswordResetTokenMapperTest extends BaseTest {
+public class PasswordResetServiceTest extends BaseTest {
 
     @Autowired
     private PasswordResetTokenMapper passwordResetTokenMapper;
@@ -28,21 +29,18 @@ public class PasswordResetTokenMapperTest extends BaseTest {
     @Autowired
     private CptmpUserMapper cptmpUserMapper;
 
+    @Autowired
+    private PasswordResetService passwordResetService;
+
     @Test
     public void test() {
-        // 创建密码重置token
-        PasswordResetToken passwordResetToken = new PasswordResetToken();
-        passwordResetToken.setGmtCreate(new Date());
-        // 设置UUID的Token
-        passwordResetToken.setToken(UUID.randomUUID().toString());
-        passwordResetToken.setEmail("123@qq.com");
-
+        String userEmail = "123@qq.com";
         // 创建用户
         CptmpUser cptmpUser = new CptmpUser();
         cptmpUser.setGmtCreate(new Date());
         cptmpUser.setUsername("test1");
         cptmpUser.updatePassword("123456");
-        cptmpUser.setEmail("123@qq.com");
+        cptmpUser.setEmail(userEmail);
         cptmpUser.setRoleName("ROLE_SCHOOL_TEACHER");
         cptmpUser.setEnabled(true);
         cptmpUser.setAccountNonExpired(true);
@@ -52,14 +50,21 @@ public class PasswordResetTokenMapperTest extends BaseTest {
         cptmpUserMapper.addUser(cptmpUser);
 
         passwordResetTokenMapper.removeAllPasswordResetTokens();
-        passwordResetTokenMapper.addPasswordResetToken(passwordResetToken);
+        passwordResetService.createPasswordResetTokenForUser(userEmail);
         List<PasswordResetToken> passwordResetTokens = passwordResetTokenMapper.findAllPasswordResetTokens();
-        Assertions.assertEquals(1, passwordResetTokens.size());
-        Assertions.assertNotNull(passwordResetTokens.get(0).getId());
-        Assertions.assertNotNull(passwordResetTokens.get(0).getEmail());
-        Assertions.assertNotNull(passwordResetTokens.get(0).getGmtCreate());
-        Assertions.assertNull(passwordResetTokens.get(0).getGmtModified());
-        Assertions.assertNotNull(passwordResetTokens.get(0).getToken());
+        Assertions.assertEquals(1, passwordResetTokenMapper.findAllPasswordResetTokens().size());
+        String token = passwordResetTokenMapper.findAllPasswordResetTokens().get(0).getToken();
+        Assertions.assertTrue(passwordResetService.authToken(token, userEmail));
+        Assertions.assertEquals(0, passwordResetTokenMapper.findAllPasswordResetTokens().size());
+
+        PasswordResetToken expiredPasswordResetToken = new PasswordResetToken();
+        expiredPasswordResetToken.setGmtCreate(new Date(100L));
+        expiredPasswordResetToken.setEmail(userEmail);
+        token = UUID.randomUUID().toString();
+        expiredPasswordResetToken.setToken(token);
+        passwordResetTokenMapper.addPasswordResetToken(expiredPasswordResetToken);
+        Assertions.assertFalse(passwordResetService.authToken(token, userEmail));
+        Assertions.assertEquals(1, passwordResetTokenMapper.findAllPasswordResetTokens().size());
     }
 
 }
