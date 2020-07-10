bd48a1ae8cea6c493761cd444b020d9e48b6576a
Eric_Lian <git@superexercisebook.com>
2020-07-10 13:35:30 +0800

    🚧 新增 TODO 标记

--- cptmp-web/src/main/java/io/github/octopigeon/cptmpweb/exceptionhandler/CustomResourceNotFoundException.java
+++ cptmp-web/src/main/java/io/github/octopigeon/cptmpweb/exceptionhandler/CustomResourceNotFoundException.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @RestControllerAdvice
 public class CustomResourceNotFoundException {
 
+    //TODO 未测试
     @ExceptionHandler(NoHandlerFoundException.class)
     public NoHandlerFoundException handleNoHandlerFoundException(NoHandlerFoundException e) {
         return e;
