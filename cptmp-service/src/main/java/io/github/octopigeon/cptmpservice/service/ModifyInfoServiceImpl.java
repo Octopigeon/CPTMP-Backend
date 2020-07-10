cc9abfc314d3c7554da5314dea9de3c618c1ecb9
Arthur-0 <1015982558@qq. com>
2020-07-10 20:12:29 +0800

    个人信息修改service

---
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/ModifyInfoServiceImpl.java
@@ -0,0 +1,132 @@
+package io.github.octopigeon.cptmpservice.service;
+
+import io.github.octopigeon.cptmpdao.mapper.CptmpUserMapper;
+import io.github.octopigeon.cptmpdao.mapper.EnterpriseAdminMapper;
+import io.github.octopigeon.cptmpdao.mapper.SchoolInstructorMapper;
+import io.github.octopigeon.cptmpdao.mapper.SchoolStudentMapper;
+import io.github.octopigeon.cptmpservice.dto.BaseUserInfoDTO;
+import io.github.octopigeon.cptmpservice.dto.EnterpriseAdminInfoDTO;
+import io.github.octopigeon.cptmpservice.dto.StudentInfoDTO;
+import io.github.octopigeon.cptmpservice.dto.TeacherInfoDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+/**
+ * 更改个人信息服务实现类
+ * @author Ruby
+ * @version 1.0
+ * @date 2020/07/10
+ * @last-check-in Ruby
+ * @date 2020/07/10
+ */
+
+@Service
+public class ModifyInfoServiceImpl implements ModifyInfoService{
+    @Autowired
+    private CptmpUserMapper cptmpUserMapper;
+    @Autowired
+    private EnterpriseAdminMapper enterpriseAdminMapper;
+    @Autowired
+    private SchoolStudentMapper schoolStudentMapper;
+    @Autowired
+    private SchoolInstructorMapper schoolInstructorMapper;
+
+
+    /**
+     * 修改用户信息
+     *
+     * @param baseUserInfo 修改后的用户信息
+     * @return
+     * 1:修改成功
+     * 0:id为空
+     */
+    @Override
+    public int modifyUserInfo(BaseUserInfoDTO baseUserInfo) throws Exception {
+
+        if(baseUserInfo.getUserId()==null)
+        {
+            return 0;
+        }
+
+        cptmpUserMapper.updateUserInfoByUsername(baseUserInfo.getUserName(),baseUserInfo.getNickname(),
+                new Date(),baseUserInfo.getIntroduction(),baseUserInfo.getGender());
+
+        switch (baseUserInfo.getRoleName())
+        {
+            case "ROLE_STUDENT_MASTER":
+            case "ROLE_STUDENT_PM":
+            case "ROLE_STUDENT_PO":
+            case "ROLE_STUDENT_MEMBER":
+                return modifyStudentInfo((StudentInfoDTO) baseUserInfo);
+            case "ROLE_ENTERPRISE_ADMIN":
+            case "ROLE_SYSTEM_ADMIN":
+                return modifyEnterpriseAdminInfo((EnterpriseAdminInfoDTO)baseUserInfo);
+            case "ROLE_SCHOOL_ADMIN":
+            case "ROLE_SCHOOL_TEACHER":
+                return modifyTeacherInfo((TeacherInfoDTO)baseUserInfo);
+            default:
+        }
+        return 1;
+    }
+
+    /**
+     * 修改学生信息
+     * @param studentInfo 修改后的学生信息
+     * @return
+     * 1:修改成功
+     */
+    public int modifyStudentInfo(StudentInfoDTO studentInfo) throws Exception {
+
+        try{
+            schoolStudentMapper.updateSchoolStudetnByUserId(studentInfo.getUserId(),new Date(),
+                    studentInfo.getName(),studentInfo.getStudentId(),studentInfo.getSchoolName(),studentInfo.getStudentFace());
+
+            return 1;
+
+        }catch(Exception e)
+        {
+            throw new Exception(e);
+        }
+    }
+
+    /**
+     * 修改教师信息
+     * @param teacherInfo 修改后的教师信息
+     * @return
+     * 1:修改成功
+     */
+    public int modifyTeacherInfo(TeacherInfoDTO teacherInfo) throws Exception {
+
+        try{
+            schoolInstructorMapper.updateSchoolInstructorByUserId(teacherInfo.getUserId(),new Date(),
+                    teacherInfo.getName(),teacherInfo.getEmployeeId(),teacherInfo.getSchoolName());
+
+            return 1;
+
+        }catch(Exception e)
+        {
+            throw new Exception(e);
+        }
+    }
+
+    /**
+     * 修改企业人员信息
+     * @param enterpriseAdminInfo 修改后的企业人员信息
+     * @return
+     * 1:修改成功
+     */
+    public int modifyEnterpriseAdminInfo(EnterpriseAdminInfoDTO enterpriseAdminInfo) throws Exception {
+
+        try{
+            enterpriseAdminMapper.updateEnterprseAdminByUserId(enterpriseAdminInfo.getUserId(),new Date(),
+                    enterpriseAdminInfo.getName(),enterpriseAdminInfo.getEmployeeId());
+            return 1;
+
+        }catch(Exception e)
+        {
+            throw new Exception(e);
+        }
+    }
+}
