1d2216e0c6d3a3fcfd5b244f42cb229d5181a920
Chong Wayne <eddyw2222@gmail.com>
2020-07-11 00:54:52 +0800

    :bug:
    修复Jacksonbug

--- cptmp-web/src/main/java/io/github/octopigeon/cptmpweb/controller/UserDetailsController.java
+++ cptmp-web/src/main/java/io/github/octopigeon/cptmpweb/controller/UserDetailsController.java
@@ -1,12 +1,20 @@
 package io.github.octopigeon.cptmpweb.controller;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.github.octopigeon.cptmpservice.dto.BaseUserInfoDTO;
 import io.github.octopigeon.cptmpservice.service.GetUserInfoService;
+import io.github.octopigeon.cptmpweb.bean.response.RespBean;
+import io.github.octopigeon.cptmpweb.bean.response.RespBeanWithObj;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,11 +38,24 @@ public class UserDetailsController {
      * @return 返回用户基本信息json
      */
     @GetMapping("/api/user/me/basic-info")
-    public BaseUserInfoDTO getMyBasicInfo(
+    public RespBean getMyBasicInfo(
         @RequestBody String json
     ) throws JsonProcessingException {
-        return getUserInfoService.findBaseUserInfoByUsername(new ObjectMapper().readValue(json, ObjectNode.class)
-                .get("username").asText());
+        RespBeanWithBaseUserInfoDTO respBean = new RespBeanWithBaseUserInfoDTO();
+        respBean.setBaseUserInfoDTO(getUserInfoService.findBaseUserInfoByUsername(new ObjectMapper().readValue(json, ObjectNode.class)
+                .get("username").asText()));
+        return respBean;
     }
+
+    // TODO 修改用户信息的接口
+
+}
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+class RespBeanWithBaseUserInfoDTO extends RespBean {
+
+    @JsonProperty("basic_info")
+    private BaseUserInfoDTO baseUserInfoDTO;
 
 }
