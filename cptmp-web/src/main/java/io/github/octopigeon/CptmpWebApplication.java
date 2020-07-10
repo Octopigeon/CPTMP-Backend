26f46f7fde35b3d02648f57bcea5cf8c1eab6f6e
Chong Wayne <eddyw2222@gmail.com>
2020-07-10 15:59:33 +0800

    :lock:
    配置全局方法安全

--- cptmp-web/src/main/java/io/github/octopigeon/CptmpWebApplication.java
+++ cptmp-web/src/main/java/io/github/octopigeon/CptmpWebApplication.java
@@ -2,8 +2,14 @@ package io.github.octopigeon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true
+)
 public class CptmpWebApplication {
 
     public static void main(String[] args) {
