1d15e431ad3c95f737d65e785935e038674ff6ae
Chong Wayne <eddyw2222@gmail.com>
2020-07-10 15:58:21 +0800

    :handbag:
    实训项目查询业务逻辑链

---
+++ cptmp-web/src/main/java/io/github/octopigeon/cptmpweb/controller/TrainProjectFindController.java
@@ -0,0 +1,38 @@
+package io.github.octopigeon.cptmpweb.controller;
+
+import io.github.octopigeon.cptmpservice.dto.trainproject.TrainProjectFindRespDTO;
+import io.github.octopigeon.cptmpservice.service.trainproject.find.TrainProjectEnterpriseAdminFindServiceImpl;
+import io.github.octopigeon.cptmpweb.bean.KeyWordBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author anlow
+ * @version 1.0
+ * @date 2020/7/10
+ * @last-check-in anlow
+ * @date 2020/7/10
+ */
+@RestController
+public class TrainProjectFindController {
+
+    @Autowired
+    private TrainProjectEnterpriseAdminFindServiceImpl trainProjectEnterpriseAdminFindService;
+
+    @Secured("ROLE_ENTERPRISE_ADMIN")
+    @GetMapping("/api/enterprise-admin/find/train-project")
+    public List<TrainProjectFindRespDTO> enterpriseAdminFindTrainProject(
+            @RequestBody KeyWordBean keyWord
+            ) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return trainProjectEnterpriseAdminFindService.findTrainProjects(keyWord.getKeyWord());
+    }
+
+}
