134a042ee0b1ce5b9a23e913c25571fe6e12acf2
Chong Wayne <eddyw2222@gmail.com>
2020-07-11 12:00:07 +0800

    :8ball:
    增加在已知原密码情况下修改密码的api，改GetUserInfoService为UserInfoService

--- cptmp-web/src/main/java/io/github/octopigeon/cptmpweb/controller/UserDetailsController.java
+++ cptmp-web/src/main/java/io/github/octopigeon/cptmpweb/controller/UserDetailsController.java
@@ -4,17 +4,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.github.octopigeon.cptmpservice.CptmpStatusCode;
 import io.github.octopigeon.cptmpservice.dto.BaseUserInfoDTO;
-import io.github.octopigeon.cptmpservice.service.GetUserInfoService;
+import io.github.octopigeon.cptmpservice.service.UserInfoService;
 import io.github.octopigeon.cptmpweb.bean.response.RespBean;
-import io.github.octopigeon.cptmpweb.bean.response.RespBeanWithObj;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,13 +22,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/7/10
  * 用于提供各种用户信息查询接口
  * @last-check-in anlow
- * @date 2020/7/10
+ * @date 2020/7/11
  */
 @RestController
 public class UserDetailsController {
 
     @Autowired
-    private GetUserInfoService getUserInfoService;
+    private UserInfoService userInfoService;
 
     /**
      * 根据用户名，得到用户基本信息
@@ -38,13 +36,35 @@ public class UserDetailsController {
      * @return 返回用户基本信息json
      */
     @GetMapping("/api/user/me/basic-info")
-    public RespBean getMyBasicInfo(
+    public RespBeanWithBaseUserInfoDTO getMyBasicInfo(
         @RequestBody String json
     ) throws JsonProcessingException {
         RespBeanWithBaseUserInfoDTO respBean = new RespBeanWithBaseUserInfoDTO();
-        respBean.setBaseUserInfoDTO(getUserInfoService.findBaseUserInfoByUsername(new ObjectMapper().readValue(json, ObjectNode.class)
+        respBean.setBaseUserInfoDTO(userInfoService.findBaseUserInfoByUsername(new ObjectMapper().readValue(json, ObjectNode.class)
                 .get("username").asText()));
         return respBean;
+    }
+
+    /**
+     * 修改用户密码
+     * @param json 包含用户名，用户输入的原密码，用户输入的新密码
+     * @return 返回重置是否成功的信息
+     */
+    @PutMapping("/api/user/me/password")
+    public RespBean updateUserPassword(
+            @RequestBody String json
+    ) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String username = objectMapper.readValue(json, ObjectNode.class).get("username").asText();
+        // 用户提交的原密码，马上会和数据库中的比较
+        String originPassword = objectMapper.readValue(json, ObjectNode.class).get("origin_password").asText();
+        String newPassword = objectMapper.readValue(json, ObjectNode.class).get("new_password").asText();
+        if (!userInfoService.validateOriginPassword(username, originPassword)) {
+            return RespBean.error(CptmpStatusCode.UPDATE_PASSWORD_FAILED, "wrong origin password");
+        } else {
+            userInfoService.updatePassword(username, newPassword);
+            return RespBean.ok("reset password success");
+        }
     }
 
     // TODO 修改用户信息的接口
