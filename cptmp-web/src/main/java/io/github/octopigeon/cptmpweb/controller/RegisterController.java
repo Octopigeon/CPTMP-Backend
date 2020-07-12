03a8bbeab21d7eb817768a9225287d7844bf35a7
Chong Wayne <eddyw2222@gmail.com>
2020-07-12 10:10:52 +0800

    :+1:
    企业注册api完成

--- cptmp-web/src/main/java/io/github/octopigeon/cptmpweb/controller/RegisterController.java
+++ cptmp-web/src/main/java/io/github/octopigeon/cptmpweb/controller/RegisterController.java
@@ -3,9 +3,11 @@ package io.github.octopigeon.cptmpweb.controller;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.octopigeon.cptmpservice.constantclass.CptmpRole;
 import io.github.octopigeon.cptmpservice.constantclass.CptmpStatusCode;
 import io.github.octopigeon.cptmpservice.constantclass.RoleEnum;
 import io.github.octopigeon.cptmpservice.dto.cptmpuser.BaseUserInfoDTO;
+import io.github.octopigeon.cptmpservice.dto.cptmpuser.EnterpriseAdminInfoDTO;
 import io.github.octopigeon.cptmpservice.service.userinfo.UserInfoService;
 import io.github.octopigeon.cptmpweb.bean.response.RespBean;
 import jdk.nashorn.internal.ir.ObjectNode;
@@ -13,6 +15,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.apache.ibatis.annotations.Insert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,23 +43,29 @@ public class RegisterController {
      * @param json 一个列表，每项包含username, password, email
      * @return 返回失败名单
      */
+    @Secured(CptmpRole.ROLE_SYSTEM_ADMIN)
     @PostMapping("/api/user/enterprise-admin")
     public RespBeanWithFailedRegisterList registerEnterpriseAdmin(@RequestBody String json) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         ReqBeanWithEnterpriseAdminRegisterInfo[] reqBeanWithEnterpriseAdminRegisterInfos
                 = objectMapper.readValue(json, ReqBeanWithEnterpriseAdminRegisterInfo[].class);
-        List<String> registerFailedList = new ArrayList<>();
-        for (ReqBeanWithEnterpriseAdminRegisterInfo reqBeanWithEnterpriseAdminRegisterInfo : reqBeanWithEnterpriseAdminRegisterInfos) {
+        List<Integer> registerFailedList = new ArrayList<>();
+        for (int i = 0; i < reqBeanWithEnterpriseAdminRegisterInfos.length; i++) {
+            ReqBeanWithEnterpriseAdminRegisterInfo reqBeanWithEnterpriseAdminRegisterInfo
+                    = reqBeanWithEnterpriseAdminRegisterInfos[i];
             try {
-                BaseUserInfoDTO baseUserInfoDTO = new BaseUserInfoDTO() {
-                };
-                baseUserInfoDTO.setUsername(reqBeanWithEnterpriseAdminRegisterInfo.getUsername());
-                baseUserInfoDTO.setPassword(reqBeanWithEnterpriseAdminRegisterInfo.getPassword());
-                baseUserInfoDTO.setEmail(reqBeanWithEnterpriseAdminRegisterInfo.getEmail());
-                baseUserInfoDTO.setRoleName(RoleEnum.ROLE_ENTERPRISE_ADMIN.name());
-                userInfoService.add(baseUserInfoDTO);
+                EnterpriseAdminInfoDTO enterpriseAdminInfoDTO = new EnterpriseAdminInfoDTO();
+                enterpriseAdminInfoDTO.setUsername(reqBeanWithEnterpriseAdminRegisterInfo.getUsername());
+                enterpriseAdminInfoDTO.setName((reqBeanWithEnterpriseAdminRegisterInfo.getName()));
+                enterpriseAdminInfoDTO.setPassword(reqBeanWithEnterpriseAdminRegisterInfo.getPassword());
+                enterpriseAdminInfoDTO.setEmail(reqBeanWithEnterpriseAdminRegisterInfo.getEmail());
+                // 用户名是一个前缀加工号
+                enterpriseAdminInfoDTO.setEmployeeId(reqBeanWithEnterpriseAdminRegisterInfo.getUsername().split("-")[1]);
+                enterpriseAdminInfoDTO.setRoleName(RoleEnum.ROLE_ENTERPRISE_ADMIN.name());
+                userInfoService.add(enterpriseAdminInfoDTO);
             } catch (Exception e) {
-                registerFailedList.add(reqBeanWithEnterpriseAdminRegisterInfo.getUsername());
+                // TODO 等服务层细化异常，尽量把每个条目的错误信息做出来
+                registerFailedList.add(i);
             }
         }
         if (registerFailedList.size() == 0) {
@@ -76,6 +85,7 @@ public class RegisterController {
 class ReqBeanWithEnterpriseAdminRegisterInfo {
 
     private String username;
+    private String name;
     private String password;
     private String email;
 
@@ -90,6 +100,6 @@ class RespBeanWithFailedRegisterList extends RespBean {
     }
 
     @JsonProperty("data")
-    private List<String> failedList;
+    private List<Integer> failedList;
 
 }
