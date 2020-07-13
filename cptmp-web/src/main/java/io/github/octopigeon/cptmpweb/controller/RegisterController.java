4c1e0bcd4ee562d2df66510887092bc6ce91594b
Chong Wayne <eddyw2222@gmail.com>
2020-07-14 00:02:19 +0800

    :bug:

--- cptmp-web/src/main/java/io/github/octopigeon/cptmpweb/controller/RegisterController.java
+++ cptmp-web/src/main/java/io/github/octopigeon/cptmpweb/controller/RegisterController.java
@@ -53,7 +53,7 @@ public class RegisterController {
      */
     @Secured(CptmpRole.ROLE_SYSTEM_ADMIN)
     @PostMapping("/api/user/enterprise-admin")
-    public RespBeanWithFailedRegisterList registerEnterpriseAdmin(@RequestBody String json) throws JsonProcessingException {
+    public RespBeanWithFailedList registerEnterpriseAdmin(@RequestBody String json) throws JsonProcessingException {
         return registerRole(json, CptmpRole.ROLE_ENTERPRISE_ADMIN);
     }
 
@@ -65,7 +65,7 @@ public class RegisterController {
      */
     @Secured(CptmpRole.ROLE_ENTERPRISE_ADMIN)
     @PostMapping("/api/user/teacher")
-    public RespBeanWithFailedRegisterList registerTeacher(@RequestBody String json) throws JsonProcessingException {
+    public RespBeanWithFailedList registerTeacher(@RequestBody String json) throws JsonProcessingException {
        return registerRole(json, CptmpRole.ROLE_SCHOOL_TEACHER);
     }
 
@@ -76,7 +76,7 @@ public class RegisterController {
      */
     @Secured(CptmpRole.ROLE_ENTERPRISE_ADMIN)
     @PostMapping("/api/user/student")
-    public RespBeanWithFailedRegisterList registerStudent(@RequestBody String json) throws JsonProcessingException {
+    public RespBeanWithFailedList registerStudent(@RequestBody String json) throws JsonProcessingException {
         return registerRole(json, CptmpRole.ROLE_STUDENT_MEMBER);
     }
 
@@ -86,7 +86,7 @@ public class RegisterController {
      * @param roleName 设定的角色名
      * @return 包含注册失败条目列表的信息
      */
-    private RespBeanWithFailedRegisterList registerRole(String json, String roleName) throws JsonProcessingException {
+    private RespBeanWithFailedList registerRole(String json, String roleName) throws JsonProcessingException {
         // 解析前端发来的一个json数组，每项包含username，name，password，email四项
         // common_id由用户名拆解得到，organization_id由系统管理员的organization_id决定（都为企业），对于学生和老师
         // 则为学校对应的id
@@ -121,7 +121,7 @@ public class RegisterController {
      */
     @Secured(CptmpRole.ROLE_ENTERPRISE_ADMIN)
     @PostMapping("/api/org")
-    public RespBeanWithFailedRegisterList registerOrganization(@RequestBody String json) throws JsonProcessingException {
+    public RespBeanWithFailedList registerOrganization(@RequestBody String json) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         ReqBeanWithOrganizationRegisterInfo[] reqBeanWithOrganizationRegisterInfos
                 = objectMapper.readValue(json, ReqBeanWithOrganizationRegisterInfo[].class);
@@ -141,7 +141,7 @@ public class RegisterController {
                 registerFailedList.add(i);
             }
         }
-        return RespBeanWithFailedRegisterList.report(registerFailedList);
+        return RespBeanWithFailedList.report(registerFailedList);
     }
 
     // TODO 等实训项目的表设计好了，再重新看一下这个api
@@ -152,7 +152,7 @@ public class RegisterController {
      */
     @Secured(CptmpRole.ROLE_ENTERPRISE_ADMIN)
     @PostMapping("/api/train-project")
-    public RespBeanWithFailedRegisterList registerTrainProject(@RequestBody String json) throws JsonProcessingException {
+    public RespBeanWithFailedList registerTrainProject(@RequestBody String json) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         ReqBeanWithTrainProjectRegisterInfo[] reqBeanWithTrainProjectRegisterInfos
                 = objectMapper.readValue(json, ReqBeanWithTrainProjectRegisterInfo[].class);
@@ -171,7 +171,7 @@ public class RegisterController {
                 registerFailedList.add(i);
             }
         }
-        return RespBeanWithFailedRegisterList.report(registerFailedList);
+        return RespBeanWithFailedList.report(registerFailedList);
     }
 
     /**
@@ -305,7 +305,7 @@ class RespBeanWithFailedList extends RespBean {
             return new RespBeanWithFailedList(CptmpStatusCode.OK, "all set");
         } else {
             RespBeanWithFailedList respBeanWithFailedList
-                    = new RespBeanWithFailedList(CptmpStatusCode.REGISTER_FAILED, "register failed");
+                    = new RespBeanWithFailedList(CptmpStatusCode.REGISTER_FAILED, "operation failed");
             respBeanWithFailedList.setFailedList(failedList);
             return respBeanWithFailedList;
         }
