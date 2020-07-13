d2b1b63ec01cd3c8561a9a5686d259c9b3441179
GuoHaoLi <2215228145@qq.com>
2020-07-13 14:58:33 +0800

    :tada:
    完成project Service

--- cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/trainproject/TrainProjectService.java
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/trainproject/TrainProjectService.java
@@ -4,6 +4,10 @@ import io.github.octopigeon.cptmpservice.dto.trainproject.TrainProjectDTO;
 import io.github.octopigeon.cptmpservice.service.basefileService.BaseFileService;
 import io.github.octopigeon.cptmpservice.service.basenormalservice.BaseNormalService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigInteger;
+import java.util.List;
 
 /**
  * @author 李国豪
@@ -15,4 +19,17 @@ import org.springframework.stereotype.Service;
 @Service
 public interface TrainProjectService extends BaseNormalService<TrainProjectDTO>, BaseFileService {
 
+    /**
+     * 根据名字进行模糊查找
+     * @param name 项目名
+     * @return 列表
+     */
+    List<TrainProjectDTO> findByLikeName(String name);
+
+    /**
+     * 给资源库上传文件
+     * @param file 文件
+     * @param projectId 项目Id
+     */
+    void uploadResourceLib(MultipartFile file, BigInteger projectId) throws Exception;
 }
