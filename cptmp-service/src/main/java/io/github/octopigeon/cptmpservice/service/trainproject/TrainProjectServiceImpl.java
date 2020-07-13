2d40e2e2a22f1b506f09d805e74d753feea2c206
GuoHaoLi <2215228145@qq.com>
2020-07-13 14:58:33 +0800

    :tada:
    完成project Service

--- cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/trainproject/TrainProjectServiceImpl.java
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/trainproject/TrainProjectServiceImpl.java
@@ -1,15 +1,24 @@
 package io.github.octopigeon.cptmpservice.service.trainproject;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import io.github.octopigeon.cptmpdao.mapper.TrainProjectMapper;
 import io.github.octopigeon.cptmpdao.model.TrainProject;
 import io.github.octopigeon.cptmpservice.config.FileProperties;
+import io.github.octopigeon.cptmpservice.dto.file.FileDTO;
 import io.github.octopigeon.cptmpservice.dto.trainproject.TrainProjectDTO;
 import io.github.octopigeon.cptmpservice.service.basefileService.BaseFileServiceImpl;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.lang.reflect.Field;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author 李国豪
@@ -20,6 +29,8 @@ import java.util.Date;
  */
 @Service
 public class TrainProjectServiceImpl extends BaseFileServiceImpl implements TrainProjectService {
+
+    private final String libJsonName = "resourceLib";
 
     @Autowired
     private TrainProjectMapper trainProjectMapper;
@@ -38,13 +49,15 @@ public class TrainProjectServiceImpl extends BaseFileServiceImpl implements Trai
     public void add(TrainProjectDTO dto) throws Exception {
         try {
             TrainProject project = new TrainProject();
+            BeanUtils.copyProperties(dto, project);
             project.setGmtCreate(new Date());
-            project.setProjectContent(dto.getContent());
-            project.setProjectLevel(dto.getLevel());
-            project.setProjectName(dto.getName());
-            project.setTrainId(dto.getTrainId());
+            JSONObject object = new JSONObject();
+            List<FileDTO> fileDTOS = new ArrayList<>();
+            object.put(this.libJsonName, fileDTOS);
+            project.setResourceLibrary(object.toJSONString());
             trainProjectMapper.addTrainProject(project);
         }catch (Exception e){
+            e.printStackTrace();
             throw new Exception("Add train project failed");
         }
     }
@@ -56,7 +69,18 @@ public class TrainProjectServiceImpl extends BaseFileServiceImpl implements Trai
      */
     @Override
     public void remove(TrainProjectDTO dto) throws Exception {
-
+        try{
+            TrainProject trainProject = trainProjectMapper.findTrainProjectById(dto.getId());
+            if(trainProject != null){
+                trainProjectMapper.removeTrainProjectById(trainProject.getId(), new Date());
+            }
+            else {
+                throw new ValueException("trainproject is not existed!");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new Exception(e);
+        }
     }
 
     /**
@@ -67,7 +91,32 @@ public class TrainProjectServiceImpl extends BaseFileServiceImpl implements Trai
      */
     @Override
     public Boolean modify(TrainProjectDTO dto) throws Exception {
-        return null;
+        try {
+            TrainProject trainProject = new TrainProject();
+            BeanUtils.copyProperties(dto, trainProject);
+            completeTrainProject(trainProject);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new Exception(e);
+        }
+    }
+
+    /**
+     * 给资源库上传文件
+     *
+     * @param file      文件
+     * @param projectId 项目Id
+     */
+    @Override
+    public void uploadResourceLib(MultipartFile file, BigInteger projectId) throws Exception {
+        FileDTO fileInfo = storePrivateFile(file);
+        TrainProject trainProject = trainProjectMapper.findTrainProjectById(projectId);
+        JSONObject object = JSON.parseObject(trainProject.getResourceLibrary());
+        List<FileDTO> resourceLib = JSON.parseArray(object.getJSONArray(this.libJsonName).toJSONString(), FileDTO.class);
+        resourceLib.add(fileInfo);
+        object.put(this.libJsonName, resourceLib);
+        trainProjectMapper.updateTrainProjectResourceById(projectId, new Date(), object.toJSONString());
     }
 
     /**
@@ -78,7 +127,48 @@ public class TrainProjectServiceImpl extends BaseFileServiceImpl implements Trai
      */
     @Override
     public TrainProjectDTO findById(BigInteger id) throws Exception {
+        TrainProject trainProject = trainProjectMapper.findTrainProjectById(id);
+        if(trainProject == null){
+            throw new ValueException("project is not existed!");
+        }
+        TrainProjectDTO trainProjectDTO = new TrainProjectDTO();
+        BeanUtils.copyProperties(trainProject, trainProjectDTO);
+        return trainProjectDTO;
+    }
 
-        return null;
+    /**
+     * 根据名字进行模糊查找
+     *
+     * @param name 项目名
+     * @return 列表
+     */
+    @Override
+    public List<TrainProjectDTO> findByLikeName(String name) {
+        List<TrainProject> trainProjects = trainProjectMapper.findTrainProjectByProjectNameAmbiguously(name);
+        List<TrainProjectDTO> trainProjectDTOS = new ArrayList<>();
+        for (TrainProject trainProject: trainProjects) {
+            TrainProjectDTO trainProjectDTO = new TrainProjectDTO();
+            BeanUtils.copyProperties(trainProject, trainProjectDTO);
+            trainProjectDTOS.add(trainProjectDTO);
+        }
+        return trainProjectDTOS;
+    }
+
+    private void completeTrainProject(TrainProject trainProject) throws IllegalAccessException {
+        TrainProject origin = trainProjectMapper.findTrainProjectById(trainProject.getId());
+        Class<? extends TrainProject> cls = trainProject.getClass();
+        Field[] fields = cls.getDeclaredFields();
+        for (Field f : fields) {
+            //设置属性可读
+            f.setAccessible(true);
+            try {
+                if (f.get(trainProject) == null) {
+                    f.set(trainProject, f.get(origin));
+                }
+            } catch (IllegalArgumentException | IllegalAccessException e) {
+                e.printStackTrace();
+                throw new IllegalAccessException();
+            }
+        }
     }
 }
