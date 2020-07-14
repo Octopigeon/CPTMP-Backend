b0294666f6839a1343b54e27ad1732687392f100
GuoHaoLi <2215228145@qq.com>
2020-07-14 21:29:26 +0800

    :construction:
    service层框架搭建

---
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/dto/team/TeamDTO.java
@@ -0,0 +1,14 @@
+package io.github.octopigeon.cptmpservice.dto.team;
+
+import lombok.Data;
+
+/**
+ * @author Gh Li
+ * @version 1.0
+ * @date 2020/7/14
+ * @last-check-in Gh Li
+ * @date 2020/7/14
+ */
+@Data
+public class TeamDTO {
+}
