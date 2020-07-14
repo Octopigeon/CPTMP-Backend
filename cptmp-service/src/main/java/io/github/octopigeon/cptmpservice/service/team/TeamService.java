9d1b70f8bfbd281a036cdc25243e13052c673f0c
GuoHaoLi <2215228145@qq.com>
2020-07-14 21:29:26 +0800

    :construction:
    service层框架搭建

---
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/team/TeamService.java
@@ -0,0 +1,14 @@
+package io.github.octopigeon.cptmpservice.service.team;
+
+import io.github.octopigeon.cptmpservice.dto.team.TeamDTO;
+import io.github.octopigeon.cptmpservice.service.basenormalservice.BaseNormalService;
+
+/**
+ * @author Gh Li
+ * @version 1.0
+ * @date 2020/7/14
+ * @last-check-in Gh Li
+ * @date 2020/7/14
+ */
+public interface TeamService extends BaseNormalService<TeamDTO> {
+}
