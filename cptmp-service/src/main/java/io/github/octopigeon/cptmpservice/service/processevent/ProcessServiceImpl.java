2c80d85fab1ebd566696fa8acf2a4e3a82e287eb
GuoHaoLi <2215228145@qq.com>
2020-07-15 18:56:18 +0800

    :construction:
    提交process service

--- cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/processevent/ProcessServiceImpl.java
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/processevent/ProcessServiceImpl.java
@@ -1,9 +1,19 @@
 package io.github.octopigeon.cptmpservice.service.processevent;
 
+import io.github.octopigeon.cptmpdao.mapper.ProcessMapper;
+import io.github.octopigeon.cptmpdao.mapper.relation.ProcessEventMapper;
+import io.github.octopigeon.cptmpdao.model.Process;
+import io.github.octopigeon.cptmpdao.model.relation.ProcessEvent;
 import io.github.octopigeon.cptmpservice.dto.processevent.ProcessDTO;
+import io.github.octopigeon.cptmpservice.utils.Utils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author Gh Li
@@ -14,6 +24,13 @@ import java.math.BigInteger;
  */
 @Service
 public class ProcessServiceImpl implements ProcessService{
+
+    @Autowired
+    private ProcessMapper processMapper;
+
+    @Autowired
+    private ProcessEventMapper processEventMapper;
+
     /**
      * 添加数据
      *
@@ -21,7 +38,15 @@ public class ProcessServiceImpl implements ProcessService{
      */
     @Override
     public void add(ProcessDTO dto) throws Exception {
-
+        try{
+            Process process = new Process();
+            BeanUtils.copyProperties(dto, process);
+            process.setGmtCreate(new Date());
+            processMapper.addProcess(process);
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new Exception(e);
+        }
     }
 
     /**
@@ -31,7 +56,9 @@ public class ProcessServiceImpl implements ProcessService{
      */
     @Override
     public void remove(ProcessDTO dto) throws Exception {
-
+        if(processMapper.findProcessById(dto.getId()) != null){
+            processMapper.removeProcessById(new Date(), dto.getId());
+        }
     }
 
     /**
@@ -42,7 +69,16 @@ public class ProcessServiceImpl implements ProcessService{
      */
     @Override
     public Boolean modify(ProcessDTO dto) throws Exception {
-        return null;
+        try{
+            Process process = new Process();
+            BeanUtils.copyProperties(dto, process, Utils.getNullPropertyNames(dto));
+            process.setGmtModified(new Date());
+            processMapper.updateProcessById(process);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new Exception(e);
+        }
     }
 
     /**
@@ -53,6 +89,67 @@ public class ProcessServiceImpl implements ProcessService{
      */
     @Override
     public ProcessDTO findById(BigInteger id) throws Exception {
-        return null;
+        Process process = processMapper.findProcessById(id);
+        ProcessDTO processDTO = new ProcessDTO();
+        BeanUtils.copyProperties(process, processDTO);
+        return processDTO;
+    }
+
+    /**
+     * 向流程添加事件
+     *
+     * @param processId 流程id
+     * @param eventId   事件id
+     */
+    @Override
+    public void addEvent(BigInteger processId, BigInteger eventId) {
+        ProcessEvent processEvent = new ProcessEvent();
+        processEvent.setGmtCreate(new Date());
+        processEvent.setEventId(eventId);
+        processEvent.setProcessId(processId);
+        processEventMapper.addProcessEvent(processEvent);
+    }
+
+    /**
+     * 从流程移除事件
+     *
+     * @param processId 流程id
+     * @param eventId   事件id
+     */
+    @Override
+    public void removeEvent(BigInteger processId, BigInteger eventId) {
+        processEventMapper.removeProcessEventByProcessIdAndEventId(processId, eventId);
+    }
+
+    /**
+     * 根据实训id查询流程
+     *
+     * @param trainId 实训id
+     * @return
+     */
+    @Override
+    public List<ProcessDTO> findByTrainId(BigInteger trainId) {
+        List<Process> processes = processMapper.findProcessesByTrainId(trainId);
+        List<ProcessDTO> results = new ArrayList<>();
+        for (Process process: processes) {
+            ProcessDTO result = new ProcessDTO();
+            BeanUtils.copyProperties(process, result);
+            results.add(result);
+        }
+        return results;
+    }
+
+    /**
+     * 根据trainId移除流程
+     *
+     * @param trainId 实训id
+     */
+    @Override
+    public void removeByTrainId(BigInteger trainId) {
+        List<Process> processes = processMapper.findProcessesByTrainId(trainId);
+        processMapper.removeProcessesByTrainId(new Date(), trainId);
+        for (Process process: processes) {
+            processEventMapper.removeProcessEventsByProcessId(process.getId());
+        }
     }
 }
