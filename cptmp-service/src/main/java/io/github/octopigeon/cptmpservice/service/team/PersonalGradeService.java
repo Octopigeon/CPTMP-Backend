2848cf326835cbbb346d0f27cf8b005ce8194569
GuoHaoLi <2215228145@qq.com>
2020-07-15 15:08:32 +0800

    :construction:
    提交personal grade service

--- cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/team/PersonalGradeService.java
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/team/PersonalGradeService.java
@@ -3,6 +3,9 @@ package io.github.octopigeon.cptmpservice.service.team;
 import io.github.octopigeon.cptmpservice.dto.team.PersonalGradeDTO;
 import io.github.octopigeon.cptmpservice.service.basenormalservice.BaseNormalService;
 
+import java.math.BigInteger;
+import java.util.List;
+
 /**
  * @author Gh Li
  * @version 1.0
@@ -11,4 +14,19 @@ import io.github.octopigeon.cptmpservice.service.basenormalservice.BaseNormalSer
  * @date 2020/7/15
  */
 public interface PersonalGradeService extends BaseNormalService<PersonalGradeDTO> {
+
+    /**
+     * 根据userId和TeamId查询个人成绩
+     * @param userId 用户Id
+     * @param teamId 团队Id
+     * @return
+     */
+    PersonalGradeDTO findByUserIdAndTeamId(BigInteger userId, BigInteger teamId);
+
+    /**
+     * 查询一个团队内所有个人成绩
+     * @param teamId 团队Id
+     * @return
+     */
+    List<PersonalGradeDTO> findByTeamId(BigInteger teamId);
 }
--- cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/team/PersonalGradeServiceImpl.java
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/team/PersonalGradeServiceImpl.java
@@ -1,11 +1,19 @@
 package io.github.octopigeon.cptmpservice.service.team;
 
+import io.github.octopigeon.cptmpdao.mapper.PersonalGradeMapper;
 import io.github.octopigeon.cptmpdao.mapper.relation.TeamPersonMapper;
+import io.github.octopigeon.cptmpdao.model.PersonalGrade;
 import io.github.octopigeon.cptmpdao.model.relation.TeamPerson;
 import io.github.octopigeon.cptmpservice.dto.team.PersonalGradeDTO;
+import io.github.octopigeon.cptmpservice.utils.Utils;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author Gh Li
@@ -19,6 +27,37 @@ public class PersonalGradeServiceImpl implements PersonalGradeService{
     @Autowired
     private TeamPersonMapper teamPersonMapper;
 
+    @Autowired
+    private PersonalGradeMapper personalGradeMapper;
+
+    /**
+     * 根据userId和TeamId查询个人成绩
+     *
+     * @param userId
+     * @param teamId
+     * @return
+     */
+    @Override
+    public PersonalGradeDTO findByUserIdAndTeamId(BigInteger userId, BigInteger teamId) {
+        return convertPersonalGrade(personalGradeMapper.findPersonalGradeByTeamPersonId(getTeamUserId(teamId, userId)));
+    }
+
+    /**
+     * 查询一个团队内所有个人成绩
+     *
+     * @param teamId 团队Id
+     * @return
+     */
+    @Override
+    public List<PersonalGradeDTO> findByTeamId(BigInteger teamId) {
+        List<TeamPerson> teamPeople = teamPersonMapper.findTeamPersonByTeamId(teamId);
+        List<PersonalGradeDTO> results = new ArrayList<>();
+        for (TeamPerson teamPerson : teamPeople) {
+            results.add(convertPersonalGrade(personalGradeMapper.findPersonalGradeById(teamPerson.getId())));
+        }
+        return results;
+    }
+
     /**
      * 添加数据
      *
@@ -26,7 +65,16 @@ public class PersonalGradeServiceImpl implements PersonalGradeService{
      */
     @Override
     public void add(PersonalGradeDTO dto) throws Exception {
-
+        try{
+            PersonalGrade personalGrade = new PersonalGrade();
+            dto.setTeamPersonId(getTeamUserId(dto.getTeamId(), dto.getUserId()));
+            BeanUtils.copyProperties(dto, personalGrade);
+            personalGrade.setGmtCreate(new Date());
+            personalGradeMapper.addPersonalGrade(personalGrade);
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new Exception(e);
+        }
     }
 
     /**
@@ -36,7 +84,11 @@ public class PersonalGradeServiceImpl implements PersonalGradeService{
      */
     @Override
     public void remove(PersonalGradeDTO dto) throws Exception {
-
+        if(personalGradeMapper.findPersonalGradeById(dto.getId()) != null){
+            personalGradeMapper.removePersonalGradeById(dto.getId(), new Date());
+        }else {
+            throw new ValueException("personalGrade is not existed!");
+        }
     }
 
     /**
@@ -47,7 +99,16 @@ public class PersonalGradeServiceImpl implements PersonalGradeService{
      */
     @Override
     public Boolean modify(PersonalGradeDTO dto) throws Exception {
-        return null;
+        try{
+            PersonalGrade personalGrade = personalGradeMapper.findPersonalGradeById(dto.getId());
+            BeanUtils.copyProperties(dto, personalGrade, Utils.getNullPropertyNames(dto));
+            personalGrade.setGmtModified(new Date());
+            personalGradeMapper.updatePersonalGradeById(personalGrade);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new Exception(e);
+        }
     }
 
     /**
@@ -58,7 +119,16 @@ public class PersonalGradeServiceImpl implements PersonalGradeService{
      */
     @Override
     public PersonalGradeDTO findById(BigInteger id) throws Exception {
-        return null;
+        return convertPersonalGrade(personalGradeMapper.findPersonalGradeById(id));
+    }
+
+    private PersonalGradeDTO convertPersonalGrade(PersonalGrade personalGrade){
+        PersonalGradeDTO personalGradeDTO = new PersonalGradeDTO();
+        BeanUtils.copyProperties(personalGrade, personalGradeDTO);
+        BigInteger[] ids = getTeamIdAndUserId(personalGrade.getTeamPersonId());
+        personalGradeDTO.setTeamId(ids[0]);
+        personalGradeDTO.setUserId(ids[1]);
+        return personalGradeDTO;
     }
 
     /**
