c62f8dcc24a234dd67839611aced643cd417776c
GuoHaoLi <2215228145@qq.com>
2020-07-16 01:12:44 +0800

    :construction:
    提交assignment service

--- cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/assignment/AssignmentService.java
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/assignment/AssignmentService.java
@@ -1,7 +1,12 @@
 package io.github.octopigeon.cptmpservice.service.assignment;
 
 import io.github.octopigeon.cptmpservice.dto.assignment.AssignmentDTO;
+import io.github.octopigeon.cptmpservice.dto.file.FileDTO;
+import io.github.octopigeon.cptmpservice.service.basefileservice.BaseFileService;
 import io.github.octopigeon.cptmpservice.service.basenormalservice.BaseNormalService;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigInteger;
 
 /**
  * @author Gh Li
@@ -10,5 +15,18 @@ import io.github.octopigeon.cptmpservice.service.basenormalservice.BaseNormalSer
  * @last-check-in Gh Li
  * @date 2020/7/14
  */
-public interface AssignmentService extends BaseNormalService<AssignmentDTO> {
+public interface AssignmentService extends BaseNormalService<AssignmentDTO>, BaseFileService {
+    /**
+     * 给资源库上传文件
+     * @param file 文件
+     * @param assignmentId 作业Id
+     */
+    void uploadResourceLib(MultipartFile file, BigInteger assignmentId) throws Exception;
+
+    /**
+     * 删除资源库中文件
+     * @param assignmentId 作业Id
+     * @param file fileDTO
+     */
+    void removeResourceLib(BigInteger assignmentId, FileDTO file) throws Exception;
 }
