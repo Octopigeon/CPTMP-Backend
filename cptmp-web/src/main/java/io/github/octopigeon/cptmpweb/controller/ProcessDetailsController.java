c5f9b3e0457e5e8d3afde03d15ffb2eac3b5e5f7
Arthur-0 <1015982558@qq.com>
2020-07-16 17:10:48 +0800

    add"getUserInfoById"

---
+++ cptmp-web/src/main/java/io/github/octopigeon/cptmpweb/controller/ProcessDetailsController.java
@@ -0,0 +1,16 @@
+package io.github.octopigeon.cptmpweb.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 陈若琳
+ * @version 2.0
+ * @date 2020/07/16
+ * @last-check-in 陈若琳
+ * @date 2020/07/16
+ */
+
+@RestController
+public class ProcessDetailsController {
+
+}
