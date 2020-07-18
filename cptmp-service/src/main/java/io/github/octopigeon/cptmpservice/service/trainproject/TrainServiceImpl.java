1d6badd50bde9ca6ef8dbe86bb9ed3e08119249f
Arthur-0 <1015982558@qq.com>
2020-07-18 18:07:01 +0800

    Revert ":bug:"
    This reverts commit 7c7a7805893818de21541825952bfd0849580dc1.

--- cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/trainproject/TrainServiceImpl.java
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/trainproject/TrainServiceImpl.java
@@ -1,7 +1,6 @@
 package io.github.octopigeon.cptmpservice.service.trainproject;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.github.pagehelper.PageHelper;
@@ -141,12 +140,11 @@ public class TrainServiceImpl extends BaseFileServiceImpl implements TrainServic
     public void uploadResourceLib(MultipartFile file, BigInteger trainId) throws Exception {
         FileDTO fileInfo = storePrivateFile(file);
         Train train = trainMapper.findTrainById(trainId);
-        // JSONObject object = JSON.parseObject(train.getResourceLibrary());
-        List<FileDTO> resourceLib = JSONArray.parseArray(train.getResourceLibrary(), FileDTO.class);
+        JSONObject object = JSON.parseObject(train.getResourceLibrary());
+        List<FileDTO> resourceLib = JSON.parseArray(object.getJSONArray(this.libJsonName).toJSONString(), FileDTO.class);
         resourceLib.add(fileInfo);
-        Object o = JSONObject.toJSON(resourceLib);
-        // object.put(this.libJsonName, resourceLib);
-        trainMapper.updateTrainProjectResourceById(trainId, new Date(), o.toString());
+        object.put(this.libJsonName, resourceLib);
+        trainMapper.updateTrainProjectResourceById(trainId, new Date(), JSONObject.toJSON(object).toString());
         attachmentFileService.add(fileInfo);
     }
 
@@ -158,12 +156,11 @@ public class TrainServiceImpl extends BaseFileServiceImpl implements TrainServic
     @Override
     public void removeResourceLib(BigInteger trainId, FileDTO fileDTO) throws Exception {
         Train train = trainMapper.findTrainById(trainId);
-        //JSONObject object = JSON.parseObject(train.getResourceLibrary());
-        List<FileDTO> resourceLib = JSON.parseArray(train.getResourceLibrary(), FileDTO.class);
+        JSONObject object = JSON.parseObject(train.getResourceLibrary());
+        List<FileDTO> resourceLib = JSON.parseArray(object.getJSONArray(this.libJsonName).toJSONString(), FileDTO.class);
         resourceLib.remove(fileDTO);
-        // object.put(this.libJsonName, resourceLib);
-        Object o = JSONObject.toJSON(resourceLib);
-        trainMapper.updateTrainProjectResourceById(trainId, new Date(), o.toString());
+        object.put(this.libJsonName, resourceLib);
+        trainMapper.updateTrainProjectResourceById(trainId, new Date(), JSONObject.toJSON(object).toString());
         attachmentFileService.remove(fileDTO);
         removeFile(fileDTO.getFilePath());
     }
@@ -224,9 +221,10 @@ public class TrainServiceImpl extends BaseFileServiceImpl implements TrainServic
             Train train = new Train();
             BeanUtils.copyProperties(dto, train);
             train.setGmtCreate(new Date());
+            JSONObject object = new JSONObject();
             List<FileDTO> fileDTOS = new ArrayList<>();
-            Object o = JSONObject.toJSON(fileDTOS);
-            train.setResourceLibrary(o.toString());
+            object.put(this.libJsonName, fileDTOS);
+            train.setResourceLibrary(object.toJSONString());
             trainMapper.addTrain(train);
         }catch (Exception e){
             e.printStackTrace();
