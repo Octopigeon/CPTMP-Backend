d1357d8dcee61e2d2180eb182b8447325e2e30f9
GuoHaoLi <2215228145@qq.com>
2020-07-19 11:52:46 +0800

    :construction:
    临时提交

--- cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/otherservice/PunchServiceImpl.java
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/otherservice/PunchServiceImpl.java
@@ -4,6 +4,7 @@ import org.gavaghan.geodesy.Ellipsoid;
 import org.gavaghan.geodesy.GeodeticCalculator;
 import org.gavaghan.geodesy.GlobalCoordinates;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigInteger;
 
@@ -41,10 +42,33 @@ public class PunchServiceImpl implements PunchService{
     /**
      * 采用人脸进行打卡
      *
+     * @param image    人脸文件
+     * @param username 用户名
      * @return
      */
     @Override
-    public Boolean facePunch() {
+    public Boolean facePunch(MultipartFile image, String username) {
         return null;
+    }
+
+    /**
+     * 添加人脸信息
+     *
+     * @param image    人脸图片
+     * @param username 用户名
+     */
+    @Override
+    public void addFaceInfo(MultipartFile image, String username) {
+
+    }
+
+    /**
+     * 删除人脸联系
+     *
+     * @param username 用户名
+     */
+    @Override
+    public void removeFaceInfo(String username) {
+
     }
 }
