b2ba5612a93f61045c2f0241ee203f018322896f
GuoHaoLi <2215228145@qq.com>
2020-07-20 15:19:51 +0800

    :construction:
    dto搭建

--- cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/dto/recruitment/RecruitmentDTO.java
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/dto/recruitment/RecruitmentDTO.java
@@ -1,6 +1,10 @@
 package io.github.octopigeon.cptmpservice.dto.recruitment;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+
+import java.math.BigInteger;
+import java.util.Date;
 
 /**
  * @author Gh Li
@@ -11,4 +15,13 @@ import lombok.Data;
  */
 @Data
 public class RecruitmentDTO {
+    private BigInteger id;
+    @JsonProperty("start_time")
+    private Date startTime;
+    @JsonProperty("end_time")
+    private Date endTime;
+    private String photo;
+    private String title;
+    @JsonProperty("website_url")
+    private String websiteUrl;
 }
