c6a7950d9a3cab942cc8f1845898849077345c27
GuoHaoLi <2215228145@qq.com>
2020-07-20 15:05:30 +0800

    :construction:
    框架搭建

---
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/dto/recruitment/RecruitmentDTO.java
@@ -0,0 +1,14 @@
+package io.github.octopigeon.cptmpservice.dto.recruitment;
+
+import lombok.Data;
+
+/**
+ * @author Gh Li
+ * @version 1.0
+ * @date 2020/7/20
+ * @last-check-in Gh Li
+ * @date 2020/7/20
+ */
+@Data
+public class RecruitmentDTO {
+}
---
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/notice/NoticeServiceImpl.java
@@ -0,0 +1,11 @@
+package io.github.octopigeon.cptmpservice.service.notice;
+
+/**
+ * @author Gh Li
+ * @version 1.0
+ * @date 2020/7/20
+ * @last-check-in Gh Li
+ * @date 2020/7/20
+ */
+public class NoticeServiceImpl {
+}
