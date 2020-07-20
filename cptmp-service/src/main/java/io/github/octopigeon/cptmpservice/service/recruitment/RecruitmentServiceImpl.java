9db7f59a8b259bc5fddf27f8bdb9e17d5e104a5a
GuoHaoLi <2215228145@qq.com>
2020-07-20 15:05:30 +0800

    :construction:
    框架搭建

---
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/recruitment/RecruitmentServiceImpl.java
@@ -0,0 +1,11 @@
+package io.github.octopigeon.cptmpservice.service.recruitment;
+
+/**
+ * @author Gh Li
+ * @version 1.0
+ * @date 2020/7/20
+ * @last-check-in Gh Li
+ * @date 2020/7/20
+ */
+public class RecruitmentServiceImpl {
+}
