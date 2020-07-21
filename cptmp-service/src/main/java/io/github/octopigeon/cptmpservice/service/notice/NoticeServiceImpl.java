9f05e9440671ea0967ef35dd94d56b08cc80b39a
GuoHaoLi <2215228145@qq.com>
2020-07-21 12:07:45 +0800

    :construction:

--- cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/notice/NoticeServiceImpl.java
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/service/notice/NoticeServiceImpl.java
@@ -1,10 +1,20 @@
 package io.github.octopigeon.cptmpservice.service.notice;
 
 import com.github.pagehelper.PageInfo;
+import io.github.octopigeon.cptmpdao.mapper.NoticeMapper;
+import io.github.octopigeon.cptmpdao.model.Notice;
 import io.github.octopigeon.cptmpservice.dto.notice.NoticeDTO;
+import io.github.octopigeon.cptmpservice.utils.Utils;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author Gh Li
@@ -15,26 +25,46 @@ import java.math.BigInteger;
  */
 @Service
 public class NoticeServiceImpl implements NoticeService{
+
+    @Autowired
+    private NoticeMapper noticeMapper;
+
     /**
      * 根据接收者去获取通知
-     *
+     * @param page 页号
+     * @param offset 页容量
      * @param receiverId 接受者Id
      * @return
      */
     @Override
-    public PageInfo<NoticeDTO> findByReceiverId(BigInteger receiverId) {
-        return null;
+    public PageInfo<NoticeDTO> findByReceiverId(int page, int offset, BigInteger receiverId) {
+        List<Notice> notices = noticeMapper.findNoticeByReceiverId(receiverId);
+        return getNoticeDTOPageInfo(notices);
     }
 
     /**
      * 根据团队Id获取通知
-     *
+     * @param page 页号
+     * @param offset 页容量
      * @param teamId 团队Id
      * @return
      */
     @Override
-    public PageInfo<NoticeDTO> findByTeamId(BigInteger teamId) {
-        return null;
+    public PageInfo<NoticeDTO> findByTeamId(int page, int offset, BigInteger teamId) {
+        List<Notice> notices = noticeMapper.findNoticeByTeamId(teamId);
+        return getNoticeDTOPageInfo(notices);
+    }
+
+
+    @NotNull
+    private PageInfo<NoticeDTO> getNoticeDTOPageInfo(List<Notice> notices) {
+        List<NoticeDTO> results = new ArrayList<>();
+        for (Notice notice: notices) {
+            NoticeDTO result = new NoticeDTO();
+            BeanUtils.copyProperties(notice, result);
+            results.add(result);
+        }
+        return new PageInfo<>(results);
     }
 
     /**
@@ -44,17 +74,32 @@ public class NoticeServiceImpl implements NoticeService{
      */
     @Override
     public void add(NoticeDTO dto) throws Exception {
-
+        Notice notice = new Notice();
+        BeanUtils.copyProperties(dto, notice);
+        notice.setGmtCreate(new Date());
+        //默认未读
+        notice.setIsRead(false);
+        noticeMapper.addNotice(notice);
     }
 
     /**
      * 移除数据
-     *
+     * 此处采用的软删除
      * @param dto ：dto实体
      */
     @Override
     public void remove(NoticeDTO dto) throws Exception {
+        if(noticeMapper.findNoticeById(dto.getId()) != null){
+            noticeMapper.hideNoticeById(dto.getId(), new Date());
+        }
+    }
 
+    /**
+     * 移除已读且超过30天的过期消息
+     */
+    @Override
+    public void removeExpiredNotice() {
+        noticeMapper.removeExpiredNotices();
     }
 
     /**
@@ -65,7 +110,19 @@ public class NoticeServiceImpl implements NoticeService{
      */
     @Override
     public Boolean modify(NoticeDTO dto) throws Exception {
-        return null;
+        try{
+            Notice notice = noticeMapper.findNoticeById(dto.getId());
+            if(notice == null){
+                throw new ValueException("Notice is not exist!");
+            }
+            BeanUtils.copyProperties(dto, notice, Utils.getNullPropertyNames(dto));
+            notice.setGmtModified(new Date());
+            noticeMapper.updateNoticeById(notice);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new Exception(e);
+        }
     }
 
     /**
@@ -76,6 +133,9 @@ public class NoticeServiceImpl implements NoticeService{
      */
     @Override
     public NoticeDTO findById(BigInteger id) throws Exception {
-        return null;
+        NoticeDTO noticeDTO = new NoticeDTO();
+        Notice notice = noticeMapper.findNoticeById(id);
+        BeanUtils.copyProperties(notice, noticeDTO);
+        return noticeDTO;
     }
 }
