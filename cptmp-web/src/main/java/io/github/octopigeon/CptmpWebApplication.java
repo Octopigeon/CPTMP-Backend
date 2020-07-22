da8716248f4fdbc9874d06652bd153e8f38ae4c0
GuoHaoLi <2215228145@qq.com>
2020-07-22 21:46:57 +0800

    :bug:

--- cptmp-web/src/main/java/io/github/octopigeon/CptmpWebApplication.java
+++ cptmp-web/src/main/java/io/github/octopigeon/CptmpWebApplication.java
@@ -1,12 +1,10 @@
 package io.github.octopigeon;
 
-import io.github.octopigeon.cptmpservice.config.ApplicationContextUtil;
 import io.github.octopigeon.cptmpservice.config.FaceProperties;
 import io.github.octopigeon.cptmpservice.config.FileProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
@@ -21,8 +19,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 public class CptmpWebApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(CptmpWebApplication.class, args);
-        ApplicationContextUtil.setApplicationContext(applicationContext);
+        SpringApplication.run(CptmpWebApplication.class, args);
     }
 
 }
