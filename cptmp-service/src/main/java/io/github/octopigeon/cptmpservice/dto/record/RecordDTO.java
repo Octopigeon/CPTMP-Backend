58f63c8e7ebf1b2f267ea7d1b562af011ddd577a
Arthur-0 <1015982558@qq.com>
2020-07-22 20:14:21 +0800

    :sparkles:

--- cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/dto/record/RecordDTO.java
+++ cptmp-service/src/main/java/io/github/octopigeon/cptmpservice/dto/record/RecordDTO.java
@@ -1,6 +1,7 @@
 package io.github.octopigeon.cptmpservice.dto.record;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.math.BigInteger;
@@ -10,18 +11,25 @@ import java.util.Date;
  * @author 李国豪
  * @version 1.0
  * @date 2020/7/14
- * @last-check-in 李国豪
- * @date 2020/7/14
+ * @last-check-in 陈若琳
+ * @date 2020/7/22
  */
 @Data
 public class RecordDTO {
     private BigInteger id;
+    @JsonIgnore
     private Date gmtCreate;
+    @JsonProperty("train_id")
     private BigInteger trainId;
+    @JsonProperty("team_id")
     private BigInteger teamId;
+    @JsonProperty("user_id")
     private BigInteger userId;
+    @JsonProperty("process_id")
     private BigInteger processId;
+    @JsonProperty("event_id")
     private BigInteger eventId;
+    @JsonProperty("assignments_lib")
     private String assignmentsLib;
     @JsonIgnore
     private BigInteger processEventId;
